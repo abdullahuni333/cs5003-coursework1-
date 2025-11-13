0af6a82b1f3afdf40d81b83502cfc46a09ed8e43
abdullahuni333 <abdullahalnahasbusiness@gmail.com>
2025-11-13 21:07:35 +0000

    resetting the project

---
+++ cs5003CW1/src/main/java/com/mycompany/cs5003cw1/Cs5003CW1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cs5003cw1;
+
+/**
+ *
+ * @author agant
+ */
+public class Cs5003CW1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
